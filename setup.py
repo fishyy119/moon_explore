5bf1056f4b97c89ebc9005666a3c86ed71db55dc
yyy <1830979240@qq.com>
2025-03-31 09:39:32 +0800

    构建ROS包

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = "moon_explore"
+
+data_files = []
+data_files.append(("share/ament_index/resource_index/packages", ["resource/" + package_name]))
+data_files.append(("share/" + package_name + "/resource", ["map_passable.npy"]))
+data_files.append(("share/" + package_name, ["package.xml", "GLOBAL.py"]))
+
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=data_files,
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="yyy",
+    maintainer_email="1830979240@qq.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
